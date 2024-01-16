00a08bacb65ffc96325e65b316e5e448be821962
Gumani24 <nengudzaernesto@gmail.com>
2024-01-16 12:07:36 -0800

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """queries the Reddit API and returns the number of subscribers"""
+
 import requests
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(subreddit)
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
