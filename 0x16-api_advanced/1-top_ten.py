aed8df03799d68b502324d475d3919b7741fbbd4
Gumani24 <nengudzaernesto@gmail.com>
2024-06-18 20:55:06 +0200

    api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+prints hot posts on a given Reddit subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    # Construct the URL for the subreddit's hot posts in JSON format
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    params = {
+        "limit": 10
+    }
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    # Check if the response status code indicates a not-found error (404)
+    if response.status_code == 404:
+        print("None")
+        return
+
+    results = response.json().get("data")
+
+    # Print the titles of the top 10 hottest posts
+    [print(c.get("data").get("title")) for c in results.get("children")]
